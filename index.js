c315281c14e5d057274cc578f682da6e15550059
Roman Jędras <romanjedras@o2.pl>
2019-04-12 20:58:34 +0200

    Fix space between code

--- index.js
+++ index.js
@@ -5,48 +5,6 @@ const EventEmitter = require('events').EventEmitter;
 const OSinfo = require('./modules/OSinfo');
 
 process.stdin.setEncoding('utf-8');
-const StatMode = require('stat-mode');
-
-// fs.stat('./cat.jpg', function(err, stats) {
-//     let statMode = new StatMode(stats);
-//     console.log(statMode.toString());
-// });
-
-// fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-//     console.log('Dane przed zapisem!'.blue);
-//     console.log(data);
-//     fs.writeFile('./tekst.txt', 'A tak wyglądają po zapisie!', function(err) {
-//         if (err) throw err;
-//         console.log('Zapisano!'.blue);
-//         fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-//             console.log('Dane po zapisie'.blue)
-//             console.log(data);
-//         });
-//     });
-// });
-// fs.exists(path.join(__dirname,"files","lorem.txt"),function (exists) {
-//
-//     if (exists) {
-//         console.log("Plik istnieje");
-//     } else {
-//         console.log("Plik nie istnieje");
-//     }
-//
-// });
-
-// fs.stat(path.join(__dirname,"files","lorem2.txt"),function (err,stats) {
-//
-//     if (err) {
-//         console.log(`Exist exceptenion:  ${err.message} `);
-//         throw err.path;
-//     }
-//
-//     console.log(`Data uyworzenia: ${stats.birthtime} `);
-//     console.log(`Data ostatniej modyfikacji: ${stats.mtime} `);
-//     console.log(`isFile: ${stats.isFile()} `);
-//     console.log(`isDirectory: ${stats.isDirectory()} `);
-// });
-
 
 const params = {
     info1 : [],
@@ -83,56 +41,10 @@ fs.readdir(path.join(__dirname,"files"),function (err, files) {
 
         })
     })
-
-
 });
 
 
 
 
-//
- const emitter = new EventEmitter();
 
-emitter.on('beforeCommand', function(instruction) {
-    console.log('You wrote: ' + instruction + ' trying to run command.')
-});
-emitter.on('afterCommand', function() {
-    console.log('Finished command');
-});
-
-
-
-process.stdin.on('readable', function() {
-    // tutaj treść tego co ma się wykonać w momencie odczytania wejścia.
-    let input = process.stdin.read();
-    if(input !== null) {
-        let instruction = input.toString().trim();
-        // odpalanie zdarzenia beforeCommand (z parametrem)
-        emitter.emit('beforeCommand', instruction);
-
-        switch (instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-             break;
-            case '/envp':
-                process.stdout.write('Info about  number of processors :' + process.env.NUMBER_OF_PROCESSORS + '\n' );
-                console.log(process.env);
-             break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            case '/getOSinfo':
-                OSinfo.print();
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        }
-
-        // emitowanie zdarzenia afterCommand (bez parametru)
-        emitter.emit('afterCommand');
-
-    }
-
-});
 
