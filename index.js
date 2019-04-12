914b924153ec3b6347d39cb773ae63893928f8da
Roman Jędras <romanjedras@o2.pl>
2019-04-12 20:51:10 +0200

    Add info about http header

--- index.js
+++ index.js
@@ -1,90 +1,38 @@
 
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
 const EventEmitter = require('events').EventEmitter;
 const OSinfo = require('./modules/OSinfo');
 
 process.stdin.setEncoding('utf-8');
 const StatMode = require('stat-mode');
 
-// fs.stat('./cat.jpg', function(err, stats) {
-//     let statMode = new StatMode(stats);
-//     console.log(statMode.toString());
-// });
+const server = http.createServer();
+server.on('request', function (request, response) {
 
-// fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-//     console.log('Dane przed zapisem!'.blue);
-//     console.log(data);
-//     fs.writeFile('./tekst.txt', 'A tak wyglądają po zapisie!', function(err) {
-//         if (err) throw err;
-//         console.log('Zapisano!'.blue);
-//         fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-//             console.log('Dane po zapisie'.blue)
-//             console.log(data);
-//         });
-//     });
-// });
-// fs.exists(path.join(__dirname,"files","lorem.txt"),function (exists) {
-//
-//     if (exists) {
-//         console.log("Plik istnieje");
-//     } else {
-//         console.log("Plik nie istnieje");
-//     }
-//
-// });
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
 
-// fs.stat(path.join(__dirname,"files","lorem2.txt"),function (err,stats) {
-//
-//     if (err) {
-//         console.log(`Exist exceptenion:  ${err.message} `);
-//         throw err.path;
-//     }
-//
-//     console.log(`Data uyworzenia: ${stats.birthtime} `);
-//     console.log(`Data ostatniej modyfikacji: ${stats.mtime} `);
-//     console.log(`isFile: ${stats.isFile()} `);
-//     console.log(`isDirectory: ${stats.isDirectory()} `);
-// });
+    response.write(`${request.httpVersion} ${request.method}`);
+    response.write("<br>");
+    response.write(`<pre>${JSON.stringify(request.headers,null,4)}</pre>`);
 
+    if (request.method === 'GET' && request.url === '/hello') {
 
-const params = {
-    info1 : [],
-    info2 : [],
-    info3 : []
-};
-
-
-fs.readdir(path.join(__dirname,"files"),function (err, files) {
-
-    if (err) {
-        console.log(`Wystapił bład ${err.message}`);
-        throw err;
+        response.write('<h1>Hello World!</h1>');
+        response.end();
+    } else {
+        response.statusCode = 404;
+        response.write('<h1>404: Zła ścieżka!</h1>');
+        response.end();
     }
-
-    files.forEach(function (filename, index) {
-        fs.stat(path.join(__dirname,"files",filename),function (err,stats) {
+});
 
 
-            if(err) {
-                console.log(`Wystapił bład ${err.message}`);
-                throw err;
-            }
 
-             params.info1[index] = '\n' + `Informacje o plikach ${filename}`;
-             params.info2[index] =  '\n' + `Data utworzenia ${stats.birthtime.getFullYear()} `;
-             params.info3[index] =  '\n' + ` ${filename} is File: ${stats.isFile()}`;
-
-
-            fs.writeFile(path.join(__dirname,"files", "info.txt"), params.info1 +'\n' + params.info2+'\n'+params.info3, function(err) {
-                if (err) throw err; // jeśli pojawi się błąd, wyrzuć wyjątek
-                console.log('Zapisano!');
-            });
-
-        })
-    })
-
-
+server.listen(9000, function () {
+    console.log('Serwer został uruchomiony na porcie http://localhost:9000')
 });
 
 
