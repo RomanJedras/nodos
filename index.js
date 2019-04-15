ee7aaa3de71dd26faa7af733d53eb9f6bcfc89a4
Roman Jędras <romanjedras@o2.pl>
2019-04-15 19:48:16 +0200

    Prepared task to display static files index / 404

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 
 const http = require('http');
-// const fs = require('fs');
-// const path = require('path');
-
+const color = require('colors');
+const fs = require('fs');
+const path = require('path');
+const pathFiles = path.join(__dirname, '.') + '\\';
 
 
 process.stdin.setEncoding('utf-8');
 
 
 const server = http.createServer();
+let html, header;
 
 server.on('request', function (request, response) {
 
     response.setHeader("Content-Type", "text/html; charset=utf-8");
 
-    response.write(`${request.httpVersion} ${request.method}`);
-    response.write("<br>");
-    response.write(`<pre>${JSON.stringify(request.headers,null,4)}</pre>`);
-
-    if (request.method === 'GET' && request.url === '/hello') {
-
-        response.write('<h1>Hello World!</h1>');
-        response.end();
+    if (request.method === 'GET' && request.url === '/') {
+    	html = fs.readFileSync(pathFiles + 'index.html','utf8');
+	    let header = 'Witaj Świecie';
+    	html = html.replace('{ Header }', header);
+    	response.end(html);
     } else {
         response.statusCode = 404;
-        response.write('<h1>404: Zła ścieżka!</h1>');
-        response.end();
+        
+        html = fs.readFileSync(pathFiles + 'errror.html');
+	    response.end(html);
     }
-});
+}).listen(9000);
+
+console.log('Server run on port : 9000'.blue);
 
 
